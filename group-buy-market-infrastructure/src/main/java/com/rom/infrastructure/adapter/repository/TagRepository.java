9d2e0d2d35bb88112363bc28b65ab5a50b7ac25c
rom <2806649895@qq.com>
2025-03-21 01:54:27 +0800

    UPDATE

--- group-buy-market-infrastructure/src/main/java/com/rom/infrastructure/adapter/repository/TagRepository.java
+++ group-buy-market-infrastructure/src/main/java/com/rom/infrastructure/adapter/repository/TagRepository.java
@@ -5,6 +5,8 @@ import com.rom.domain.tag.model.entity.CrowdTagsJobEntity;
 import com.rom.infrastructure.dao.ICrowdTagsDao;
 import com.rom.infrastructure.dao.ICrowdTagsDetailDao;
 import com.rom.infrastructure.dao.ICrowdTagsJobDao;
+import com.rom.infrastructure.dao.po.CrowdTagsJob;
+import com.rom.infrastructure.redis.IRedisService;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
@@ -18,9 +20,22 @@ public class TagRepository implements ITagRepository {
     @Resource
     private ICrowdTagsDetailDao crowdTagsDetailDao;
 
+    @Resource
+    private IRedisService redisService;
     @Override
     public CrowdTagsJobEntity queryCrowdTagsJobEntity(String tagId, String batchId) {
-        return null;
+        CrowdTagsJob crowdTagsJobReq = new CrowdTagsJob();
+        crowdTagsJobReq.setTagId(tagId);
+        crowdTagsJobReq.setBatchId(batchId);
+
+        CrowdTagsJob crowdTagsJobRes = crowdTagsJobDao.queryCrowdTagsJob(crowdTagsJobReq);
+        if (null == crowdTagsJobRes) return null;
+        return CrowdTagsJobEntity.builder()
+                .tagType(crowdTagsJobRes.getTagType())
+                .tagRule(crowdTagsJobRes.getTagRule())
+                .statStartTime(crowdTagsJobRes.getStatStartTime())
+                .statEndTime(crowdTagsJobRes.getStatEndTime())
+                .build();
     }
 
     @Override
