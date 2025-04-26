4ff3811e28da0030a34df9fa79b512fc375d145d
lrqiisrom <2806649895@qq.com>
2025-04-26 19:32:04 +0800

    修正日期问题

--- group-buy-market-api/src/main/java/com/rom/api/dto/SettlementMarketPayOrderRequestDTO.java
+++ group-buy-market-api/src/main/java/com/rom/api/dto/SettlementMarketPayOrderRequestDTO.java
@@ -1,6 +1,6 @@
 package com.rom.api.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 import java.util.Date;
@@ -19,6 +19,5 @@ public class SettlementMarketPayOrderRequestDTO {
     /** 外部交易单号-确保外部调用唯一幂等 */
     private String outTradeNo;
     /** 外部交易时间 */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date outTradeTime;
 }
