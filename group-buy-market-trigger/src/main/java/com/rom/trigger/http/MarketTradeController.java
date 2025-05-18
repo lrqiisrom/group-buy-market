4c06c0638e552a22abb624b5bbf3f4cd57f392e8
lrqiisrom <2806649895@qq.com>
2025-05-18 18:06:15 +0800

    修正外部调用返回结果

--- group-buy-market-trigger/src/main/java/com/rom/trigger/http/MarketTradeController.java
+++ group-buy-market-trigger/src/main/java/com/rom/trigger/http/MarketTradeController.java
@@ -136,7 +136,9 @@ public class MarketTradeController implements IMarketTradeService {
                     .info(ResponseCode.SUCCESS.getInfo())
                     .data(LockMarketPayOrderResponseDTO.builder()
                             .orderId(marketPayOrderEntity.getOrderId())
+                            .originalPrice(marketPayOrderEntity.getOriginalPrice())
                             .deductionPrice(marketPayOrderEntity.getDeductionPrice())
+                            .payPrice(marketPayOrderEntity.getPayPrice())
                             .tradeOrderStatus(marketPayOrderEntity.getTradeOrderStatusEnumVO().getCode())
                             .build())
                     .build();
