1d3a040a46b966208a2f863e0297f4f2fd85a0bb
lrqiisrom <2806649895@qq.com>
2025-05-21 23:17:57 +0800

    rabbitMQ对接

--- group-buy-market-app/src/test/java/com/rom/test/ApiTest.java
+++ group-buy-market-app/src/test/java/com/rom/test/ApiTest.java
@@ -1,30 +1,40 @@
 package com.rom.test;
 
+import com.rom.infrastructure.event.EventPublisher;
 import com.rom.infrastructure.redis.IRedisService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.redisson.api.RBitSet;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
 
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApiTest {
     @Resource
-    private IRedisService redisService;
+    private EventPublisher publisher;
+
+    @Value("${spring.rabbitmq.config.producer.topic_team_success.routing_key}")
+    private String routingKey;
 
     @Test
-    public void test_redis() {
-        RBitSet bitSet = redisService.getBitSet("redis");
-        System.out.println(bitSet.isExists());
-    }
-    @Test
-    public void test() {
-        log.info("测试完成");
+    public void test_rabbitmq() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        publisher.publish(routingKey, "订单结算：ORD-20231234");
+        publisher.publish(routingKey, "订单结算：ORD-20231235");
+        publisher.publish(routingKey, "订单结算：ORD-20231236");
+        publisher.publish(routingKey, "订单结算：ORD-20231237");
+        publisher.publish(routingKey, "订单结算：ORD-20231238");
+
+        // 等待，消息消费。测试后，可主动关闭。
+        countDownLatch.await();
     }
 
 }
